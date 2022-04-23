61caaa8b768b962e2ccee8a45a095f4b29de52ce
puja <pujarana1796@gmail.com>
2022-04-23 11:03:14 +0530

    fibonacci series

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace LogicalProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("logicalProgrammimg");
+            fibonnaci f1 = new fibonnaci();
+            f1.series();
+            
+        }
+    }
+
+}
