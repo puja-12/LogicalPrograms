a5f41659461fc818244b8b0d445ec6d20f440794
puja <pujarana1796@gmail.com>
2022-04-25 22:03:23 +0530

    stopwatch

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalProgram
         {
             while (true)
             {
-                Console.WriteLine("select no\n1)fibonacci series\n2)perfect number\n3)Prime number\n");
+                Console.WriteLine("select no\n1)fibonacci series\n2)perfect number\n3)Prime number\n4)stopwatch\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -24,6 +24,10 @@ namespace LogicalProgram
                     case 3:
                         primeNo prime = new primeNo();
                         prime.primeNumber();
+                        break;
+                    case 4:
+                        Demo_Stopwatchh W1 = new Demo_Stopwatchh();
+                        W1.watch();
                         break;
                 }
             }
---
+++ LogicalProgram/stopwatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading; 
+class Demo_Stopwatchh 
+{
+    public void watch()
+    {
+        Stopwatch stopw = new Stopwatch();
+        stopw.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(2);
+        }
+        stopw.Stop();
+        Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
+    }
+}
